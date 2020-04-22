82d76a0f46c6f6737cf854a30102d258ffa66a29
ananyasmirti <43774146+ananyasmirti@users.noreply.github.com>
2020-04-22 21:44:43 +0530

    commit

--- entries/views.py
+++ entries/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView
 from .models import Entry
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-
+from django.shortcuts import redirect
 from django.urls import reverse_lazy
 from django.views.generic.edit import DeleteView,UpdateView
-
+from django.contrib import messages
 
 
 
@@ -38,16 +38,31 @@ from django.urls import reverse_lazy
 
 
 
-class YDeleteView(  DeleteView,):
+class YDeleteView(LoginRequiredMixin,  DeleteView,):
     model = Entry
     success_url = reverse_lazy('blog-home')
+  
+    def dispatch(self, request, *args, **kwargs):
+    
+        obj = self.get_object()
+        if obj.entry_author != self.request.user:
+            messages.error(request, 'Document not deleted.')
+            return redirect('blog-home')
+        messages.success(request, 'Document deleted.')
+        return super(YDeleteView, self).dispatch(request, *args, **kwargs)
 
 
-class EditPost(UpdateView):
+class EditPost(LoginRequiredMixin,UpdateView):
     model = Entry
     template_name = 'entries/create_entry.html'
     fields = ['entry_title','entry_text']
     success_url = reverse_lazy('blog-home')
-    
+    def dispatch(self, request, *args, **kwargs):
+   
+        obj = self.get_object()
+        if obj.entry_author != self.request.user:
+            messages.error(request, 'Document does not belong to you')
+            return redirect('blog-home')
+        return super(EditPost, self).dispatch(request, *args, **kwargs)
    
     
